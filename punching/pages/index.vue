4978c0b03611ce14a4e7eac8ae6d88678a49a133
wkk5194 <kysoowjk@163.com>
2020-01-22 21:25:51 +0800

    首页

--- punching/pages/index.vue
+++ punching/pages/index.vue
@@ -1,68 +1,68 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        punching
-      </h1>
-      <h2 class="subtitle">
-        word punching project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+    <div class="mainbox">
+      <el-row>
+        <div class="title">Words Punching</div>
+      </el-row>
+      <el-row>
+        <el-col :span="10" :offset="6">
+          <el-input v-model="input" placeholder="Words To Add"></el-input>
+        </el-col>
+        <el-col :span="2">
+          <el-button type="primary">Add</el-button>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="4" :offset="10">
+          <div class="btn-links">
+            <el-button type="text">Statistics</el-button>
+            <el-button type="text">Words-List</el-button>
+          </div>
+        </el-col>
+      </el-row>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
-  components: {
-    Logo
+  components: {},
+  data() {
+    return {
+      input: ''
+    }
   }
 }
 </script>
 
 <style>
 .container {
-  margin: 0 auto;
   min-height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
   text-align: center;
-}
-
-.title {
   font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
     'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
 }
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+.mainbox {
+  width: 100vw;
 }
-
-.links {
-  padding-top: 15px;
+.title {
+  font-size: 60px;
+  margin-bottom: 80px;
+}
+.input-item {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+}
+.btn-links {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  margin-top: 30px;
 }
 </style>
