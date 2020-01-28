3ceea2cd8760d2193cf9c75ccfcbb83911c08809
wkk5194 <kysoowjk@163.com>
2020-01-28 23:52:47 +0800

    debug docker env

--- punching/nuxt.config.js
+++ punching/nuxt.config.js
@@ -41,7 +41,8 @@ module.exports = {
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
-    '@nuxtjs/pwa'
+    '@nuxtjs/pwa',
+    '@nuxtjs/eslint-module'
   ],
   /*
    ** Axios module configuration
